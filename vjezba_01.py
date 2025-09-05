88c20e519db693bfbc73c4389ee659a7fadf3275
Jašo <andrija.pericic@gmail.com>
2025-09-05 21:02:09 +0200

    Create vjezba_01.py

---
+++ vjezba_01.py
@@ -0,0 +1,33 @@
+'''
+IP adresa je adresa svakog računala na mreži koja se sastoji od četiri broja između 0 i 256.
+Primjer IP adrese: 192.168.0.184
+
+IP adresu iz primjera ispišite u binarnom, oktalno i heksadekadskom obliku.
+SAVJET: Za sada koristite zasebnu varijablu za svaki od četiri broja, 
+        odnosno dijela (okteta) IP adrese, ali ispišite ih u istom obliku 
+        kako je navedeno u primjeru (192.168.0.184).
+Ispis treba napraviti za sve oblike brojevnih sustava.
+'''
+
+# Fiksno unesena IP adresa
+# a= 192
+# b = 168
+# c = 0
+# d = 184
+
+# User input IP
+
+a = int(input('Unesi 3 broja IP adrese: '))
+b = int(input('Unesi 3 broja IP adrese: '))
+c = int(input('Unesi 1 broj IP adrese: '))
+d = int(input('Unesi 3 broja IP adrese: '))
+
+
+binary = f'{bin(a)}.{bin(b)}.{bin(c)}.{bin(d)}'
+octal = f'{oct(a)}.{oct(b)}.{oct(c)}.{oct(d)}'
+hexa = f'{hex(a)}.{hex(b)}.{hex(c)}.{hex(d)}'
+
+
+print(f'Prikaz IP adrese {a}.{b}.{c}.{d} u binarnom zapisu je {binary}')
+print(f'Prikaz IP adrese {a}.{b}.{c}.{d} u oktalnom zapisu je {octal}')
+print(f'Prikaz IP adrese {a}.{b}.{c}.{d} u heksadekadskom zapisu je {hexa}')
